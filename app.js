f8a34766b827a451e9dacb74c4e0448066e4f1e2
MakerZip <makerzip@hanmail.net>
2023-06-30 10:01:41 +0900

    아마.. 작동 될걸요..?

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 const express = require('express');
+const ejs = require('ejs');
+
 const app = express();
-const PORT = 3000;
+
+// Define routes
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/projects', (req, res) => {
+  // Get all projects from the database
+  const projects = [
+    {
+      title: 'My First Project',
+      description: 'This is my first project.',
+      url: 'https://myfirstproject.com',
+    },
+    {
+      title: 'My Second Project',
+      description: 'This is my second project.',
+      url: 'https://mysecondproject.com',
+    },
+  ];
+
+  // Render the projects page
+  res.render('projects.ejs', { projects });
+});
+
+// Start the server
+app.listen(3000);
+console.log('Server started on port 3000');
